1fd9c6dd9a32845df748aca4558d5b6e9f7c7256
anny_more <anxxxen@ya.ru>
2023-01-28 15:11:12 +0300

    fix: delete comments in converterToWebp

--- src/converter.ts
+++ src/converter.ts
@@ -2,10 +2,8 @@ import {WriteStream} from 'fs';
 // eslint-disable-next-line node/no-extraneous-import
 import sharp from 'sharp';
 
-const converterToWebp = async (target: WriteStream, arg: string) => {
-  //const imageProcessing = sharp();
-
-  await sharp(arg).webp().pipe(target);
+const converterToWebp = (target: WriteStream, arg: string) => {
+  sharp(arg).webp().pipe(target);
 };
 
 export default converterToWebp;
